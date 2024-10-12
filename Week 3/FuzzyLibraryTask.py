b6d953b368863b025b50c1b3de8279463ab7e1d6
Gus <2jonea44@solent.ac.uk>
2024-10-12 22:44:33 +0100

    Implementation of TKinter GUI (Text field input)

--- Week 3/FuzzyLibraryTask.py
+++ Week 3/FuzzyLibraryTask.py
@@ -1,6 +1,10 @@
 import numpy as np
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
+import tkinter
+from tkinter import *
+from tkinter import messagebox
+
 
 complexity = ctrl.Antecedent(np.arange(0, 10, 1), 'complexity')
 timeToStudy = ctrl.Antecedent(np.arange(0, 20, 0.5), 'timeToStudy')
@@ -33,20 +37,62 @@ condition1 = ctrl.Rule(complexity['hard'] | timeToStudy['short'], additionalHour
 condition2 = ctrl.Rule(complexity['medium'], additionalHours['medium'])
 condition3 = ctrl.Rule(complexity['easy'] & timeToStudy['long'], additionalHours['low'])
 
-# User input for the simulations (So it is different almost every time)
-complexityInput = int(input(print(
-    'Enter the complexity of the task from 1-10, with 10 being the hardest\n')))
-timeToStudyInput = int(input(print(
-    'Enter the number of weeks you have left to study the topic:\n')))
 
-# Setting the simulation up
-additionalHours_ctrl = ctrl.ControlSystem([condition1, condition2, condition3])
-additionalHours_sim = ctrl.ControlSystemSimulation(additionalHours_ctrl)
+# Creating a GUI for the user to enter their values into
 
-additionalHours_sim.input['complexity'] = complexityInput
-additionalHours_sim.input['timeToStudy'] = timeToStudyInput
+class tkinterGUI:
 
-additionalHours_sim.compute()
+    def __init__(self, buttonInput):
+        self.data = buttonInput
 
-print("You should study %.2f additional hours on the subject." % additionalHours_sim.output['additionalHours'])
-additionalHours.view(sim=additionalHours_sim)
+
+buttonInput1 = tkinterGUI(None)
+buttonInput2 = tkinterGUI(None)
+
+tk = tkinter.Tk()
+tk.geometry("275x150")
+e1_var = tkinter.StringVar()
+e2_var = tkinter.StringVar()
+e1 = Entry(tk, textvariable=e1_var)
+e2 = Entry(tk, textvariable=e2_var)
+
+def getInput(button, entry):
+    button.data = entry.get()
+    print(button.data)
+    entry.set("")
+
+
+def getOutput():
+    getInput(buttonInput1, e1_var)
+    getInput(buttonInput2, e2_var)
+    startSim()
+
+
+def startSim():
+    # Setting the simulation up
+    additionalHours_ctrl = ctrl.ControlSystem([condition1, condition2, condition3])
+    additionalHours_sim = ctrl.ControlSystemSimulation(additionalHours_ctrl)
+
+    additionalHours_sim.input['complexity'] = int(buttonInput1.data)
+    additionalHours_sim.input['timeToStudy'] = int(buttonInput2.data)
+
+    additionalHours_sim.compute()
+
+    messagebox.showinfo('Result', ("Additional Study hours: %.2f" % additionalHours_sim.output['additionalHours']))
+
+
+button1 = Button(tk, text='Submit', command=getOutput)
+Label(tk, text='').grid(row=0)
+Label(tk, text='Complexity: ').grid(row=1, column=0)
+e1.grid(row=1, column=1)
+Label(tk, text='').grid(row=2)
+Label(tk, text='Time left to study: ').grid(row=3, column=0)
+e2.grid(row=3, column=1)
+Label(tk, text='').grid(row=4)
+button1.grid(row=5)
+Label(tk, text='').grid(row=6)
+tk.mainloop()
+
+# additionalHours.view(sim=additionalHours_sim)
+
+
